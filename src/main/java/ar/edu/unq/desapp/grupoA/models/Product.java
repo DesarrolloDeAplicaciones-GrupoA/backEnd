362fc6673d04c2ddf9ea12846548df5467e22e70
damian <damianlattenero@gmail.com>
2016-05-03 21:05:36 -0300

    se meppea Product

--- src/main/java/ar/edu/unq/desapp/grupoA/models/Product.java
+++ src/main/java/ar/edu/unq/desapp/grupoA/models/Product.java
@@ -1,17 +1,29 @@
 package ar.edu.unq.desapp.grupoA.models;
 
+import javax.persistence.*;
 import javax.swing.*;
 
+@Entity
+@Table(name = "UserModel")
 public class Product {
 
+    @Id()
+    @GeneratedValue()
+    @Column(name = "USER_ID")
+    private int id;
+
+    @Column(name = "name")
     private String name;
+    @Column(name = "photo")
     private ImageIcon photo;
+    @Column(name = "stock")
     private int stock;
+    @Column(name = "point_cost")
     private int pointCost;
 
     public Product(String name, int stock, int pointCost) {
         this.name = name;
-        this.stock= stock;
+        this.stock = stock;
         this.pointCost = pointCost;
     }
 
