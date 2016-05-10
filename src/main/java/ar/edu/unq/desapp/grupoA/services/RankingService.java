91c4e30ba145585e4b7b7ad6bea3be73dcff2b76
Pablo <loiaconopablo@github.com>
2016-05-10 12:03:32 -0300

    Se agrego servicios de spring al ranking service

--- src/main/java/ar/edu/unq/desapp/grupoA/services/RankingService.java
+++ src/main/java/ar/edu/unq/desapp/grupoA/services/RankingService.java
@@ -2,10 +2,12 @@ package ar.edu.unq.desapp.grupoA.services;
 
 import ar.edu.unq.desapp.grupoA.models.Ranking;
 import ar.edu.unq.desapp.grupoA.models.UserModel;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Set;
 
+@Service("rankingService")
 public class RankingService {
 
 
