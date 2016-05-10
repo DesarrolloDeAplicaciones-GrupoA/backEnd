65193bffabc0fdedf74cb7ced5e630f170d97c45
Leandro Gomez <leandro.gz73@gmail.com>
2016-05-10 16:03:56 -0300

    Agregado test de integracion para el servicio de ranking

--- src/main/java/ar/edu/unq/desapp/grupoA/services/RankingService.java
+++ src/main/java/ar/edu/unq/desapp/grupoA/services/RankingService.java
@@ -2,6 +2,8 @@ package ar.edu.unq.desapp.grupoA.services;
 
 import ar.edu.unq.desapp.grupoA.models.Ranking;
 import ar.edu.unq.desapp.grupoA.models.UserModel;
+import ar.edu.unq.desapp.grupoA.repositories.RankingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,10 +12,13 @@ import java.util.Set;
 @Service("rankingService")
 public class RankingService {
 
+    @Autowired
+    private RankingRepository rankingRepository;
 
     public Ranking createRanking(Set<UserModel> userModelSet) {
-
-        return new Ranking(userModelSet);
+        Ranking ranking =new Ranking(userModelSet);
+        this.rankingRepository.save(ranking);
+        return this.rankingRepository.findById(ranking.getId());
 
     }
 
--- src/test/java/ar/edu/unq/desapp/grupoA/services/RankingServiceTest.java
+++ src/test/java/ar/edu/unq/desapp/grupoA/services/RankingServiceTest.java
@@ -3,38 +3,55 @@ package ar.edu.unq.desapp.grupoA.services;
 
 import ar.edu.unq.desapp.grupoA.models.Ranking;
 import ar.edu.unq.desapp.grupoA.models.UserModel;
+import ar.edu.unq.desapp.grupoA.repositories.RankingRepository;
+import ar.edu.unq.desapp.grupoA.testUtis.factories.UserModelTestFactory;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Stream;
 
-public class RankingServiceTest {
+public class RankingServiceTest extends AbstractServiceTest {
 
+    @Autowired
     private RankingService rankingService;
+    @Autowired
+    private UserModelTestFactory userModelTestFactory;
+    @Autowired
+    private RankingRepository rankingRepository;
     private Ranking ranking;
 
     @Before
     public void setUp() {
         HashSet<UserModel> userSet = new HashSet<>();
-        userSet.add(new UserModel("foo bar", "fooba@@@"));
-        userSet.add(new UserModel("foo bar12", "fooba@@"));
-        userSet.add(new UserModel("foo bar13", "fooba@"));
-        rankingService = new RankingService();
-        ranking = rankingService.createRanking(userSet);
-
+        userSet.add(this.getUser());
+        userSet.add(this.getUser());
+        userSet.add(this.getUser());
+        this.ranking = this.rankingService.createRanking(userSet);
     }
 
     @Test
     public void sumarPuntosALos20MejoresRankeados() {
-        rankingService.rankearATodos(ranking);
-        Set<UserModel> usuarios = rankingService.getUsuariosEficiente(ranking);
+        this.rankingService.rankearATodos(this.ranking);
+        Set<UserModel> usuarios = this.rankingService.getUsuariosEficiente(this.ranking);
         Stream<Integer> integerStream = usuarios.stream().map((usuario -> usuario.getPoints()));
         integerStream.forEach(point -> Assert.assertEquals(point.intValue(), 5000));
+    }
+
+    @Test
+    public void hasOneMoreRanking() {
+        Assert.assertEquals(1, this.rankingRepository.count());
+
+    }
+
+    private UserModel getUser() {
+        return this.userModelTestFactory.getUser();
     }
 
 }
