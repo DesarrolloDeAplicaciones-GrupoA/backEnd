9e8de26763278e1c5107c9ef7236c563f7b37bb4
Pablo <loiaconopablo@github.com>
2016-05-10 12:34:56 -0300

    Se modifico unas cosas del agregado de vehiculo.

--- src/main/java/ar/edu/unq/desapp/grupoA/services/VehicleAdding.java
+++ src/main/java/ar/edu/unq/desapp/grupoA/services/VehicleAdding.java
@@ -3,17 +3,20 @@ package ar.edu.unq.desapp.grupoA.services;
 import ar.edu.unq.desapp.grupoA.models.UserModel;
 import ar.edu.unq.desapp.grupoA.models.Vehicle;
 import ar.edu.unq.desapp.grupoA.repositories.VehicleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service("vehicleAdding")
 public class VehicleAdding {
 
+    @Autowired
     private VehicleRepository repository;
 
     @Transactional
     public void createVehicle(UserModel user, String brand, int capacity) {
         Vehicle vehicle = new Vehicle(brand, capacity, user);
         user.setVehicle(vehicle);
+        repository.save(vehicle);
     }
 }
