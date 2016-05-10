ccb068f9da04b4c06e7729baa2af5e78ac7fb619
Leandro Gomez <leandro.gz73@gmail.com>
2016-05-10 16:03:17 -0300

    Se agregan getter y setter para la clase Ranking

--- src/main/java/ar/edu/unq/desapp/grupoA/models/Ranking.java
+++ src/main/java/ar/edu/unq/desapp/grupoA/models/Ranking.java
@@ -27,4 +27,12 @@ public class Ranking {
     public void rankearATodos() {
         usuariosEficientes.forEach(user -> user.setPoints(5000));
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
