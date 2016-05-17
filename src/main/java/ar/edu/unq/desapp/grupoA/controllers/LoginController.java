ce412046612359f19d5dd1378f3037520db7e287
Pablo <loiaconopablo@github.com>
2016-05-17 13:04:33 -0300

    Se agrego la configuracion para que audite los servicios. Crea un archivo AUDIT.LOG.HTML

--- src/main/java/ar/edu/unq/desapp/grupoA/controllers/LoginController.java
+++ src/main/java/ar/edu/unq/desapp/grupoA/controllers/LoginController.java
@@ -3,6 +3,7 @@ package ar.edu.unq.desapp.grupoA.controllers;
 import ar.edu.unq.desapp.grupoA.factories.UserModelFactory;
 import ar.edu.unq.desapp.grupoA.models.UserModel;
 import ar.edu.unq.desapp.grupoA.services.Login;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -11,6 +12,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import java.util.List;
+
+
+//import java.util.logging.Logger;
 
 @Path("userModels")
 @Controller("loginController")
@@ -23,6 +27,7 @@ public class LoginController {
     @Path("all")
     @Produces("application/json")
     public List<UserModel> getAll() {
+        log.info("Call Service and get all user");
         return this.getLogin().findAll();
     }
 
@@ -30,9 +35,12 @@ public class LoginController {
         return login;
     }
 
+    private static final Logger log = Logger.getLogger(LoginController.class.getName());
+
     @Autowired
     public void setLogin(Login login) {
         this.login = login;
+        log.info("Set Login Controller");
     }
 
 
@@ -40,6 +48,9 @@ public class LoginController {
     public void loadData() {
         this.getUserFactory().getUser();
         this.getUserFactory().getUser();
+        log.info("Finish create test users");
+        log.warn("TESTING WARNING");
+
     }
 
     public UserModelFactory getUserFactory() {
@@ -50,4 +61,6 @@ public class LoginController {
     public void setUserFactory(UserModelFactory userFactory) {
         this.userFactory = userFactory;
     }
+
+
 }
