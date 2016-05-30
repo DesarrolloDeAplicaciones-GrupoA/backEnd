8c5bc8119d7dd274df3e976558b24b081c690059
Leandro Gomez <leandro.gz73@gmail.com>
2016-05-30 13:30:01 -0300

    Se crea controller para creacion de vehiculos

---
+++ src/main/java/ar/edu/unq/desapp/grupoA/controllers/VehicleController.java
@@ -0,0 +1,48 @@
+package ar.edu.unq.desapp.grupoA.controllers;
+
+import ar.edu.unq.desapp.grupoA.controllers.requests.VehicleCreationBody;
+import ar.edu.unq.desapp.grupoA.controllers.responses.VehicleCreationResponse;
+import ar.edu.unq.desapp.grupoA.models.UserModel;
+import ar.edu.unq.desapp.grupoA.models.Vehicle;
+import ar.edu.unq.desapp.grupoA.repositories.UserTokenRepository;
+import ar.edu.unq.desapp.grupoA.services.VehicleAdding;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import javax.ws.rs.*;
+
+@Path("vehicles")
+@Controller("vehicleController")
+public class VehicleController {
+    private VehicleAdding vehicleAdding;
+    private UserTokenRepository userTokenRepository;
+
+    @POST
+    @Path("create")
+    @Consumes("application/json")
+    @Produces("application/json")
+    public VehicleCreationResponse create(@QueryParam("token") String  token, VehicleCreationBody vehicleCreationBody) {
+        UserModel user = this.getUserTokenRepository().findByUserToken(token);
+        Vehicle vehicle = this.getVehicleAdding().createVehicle(user, vehicleCreationBody.getBrand(), vehicleCreationBody.getCapacity());
+        return VehicleCreationResponse.build(vehicle);
+    }
+
+
+    public VehicleAdding getVehicleAdding() {
+        return vehicleAdding;
+    }
+
+    @Autowired
+    public void setVehicleAdding(VehicleAdding vehicleAdding) {
+        this.vehicleAdding = vehicleAdding;
+    }
+
+    public UserTokenRepository getUserTokenRepository() {
+        return userTokenRepository;
+    }
+
+    @Autowired
+    public void setUserTokenRepository(UserTokenRepository userTokenRepository) {
+        this.userTokenRepository = userTokenRepository;
+    }
+}
