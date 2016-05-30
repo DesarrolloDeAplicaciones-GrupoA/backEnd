3d6c04ba807a2ed9201b4cdff2ac77bddf7e35fe
Pablo Loiacono <loiaconopablo@gmail.com>
2016-05-30 15:45:21 -0300

    Set Correct Loger Aspect

--- src/main/java/ar/edu/unq/desapp/grupoA/aspects/LoggerAspect.java
+++ src/main/java/ar/edu/unq/desapp/grupoA/aspects/LoggerAspect.java
@@ -7,6 +7,8 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+import com.jcabi.aspects.Loggable;
+
 
 //@Component
 @Aspect
@@ -15,9 +17,11 @@ public class LoggerAspect {
     private Logger log = Logger.getLogger(getClass());
 
     //busca todos los metodos publicos del paquete XX y que tengan la annotacion Loggeable
-    //@After("execution(public * ar.edu.unq.desapp.grupoA.services.*.*(..)) && @annotation(Loggable)")
+  //  @After("execution(public * ar.edu.unq.desapp.grupoA.services.*.*(..)) && @annotation(Loggable)")
+    //@After("execution(* *(..)) && @annotation(pablin)")
+    //@After("execution (@Loggeable public * ar.edu.unq.desapp.grupoA.services.*.*(..))")
     @After("execution(public * ar.edu.unq.desapp.grupoA.services.*.*(..))")
-    public void logAfter(JoinPoint pjp) {
+    public void logAfter(JoinPoint pjp, Loggable loggable) {
         log.info(pjp.getSignature().getName() + " called...TESTING");
     }
 
