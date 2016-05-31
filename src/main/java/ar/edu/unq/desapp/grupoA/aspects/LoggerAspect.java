32c471f6132a5bd24d7981a9112ff9b6e9df00eb
Pablo Loiacono <loiaconopablo@gmail.com>
2016-05-31 18:31:13 -0300

    Corrección del back

--- src/main/java/ar/edu/unq/desapp/grupoA/aspects/LoggerAspect.java
+++ src/main/java/ar/edu/unq/desapp/grupoA/aspects/LoggerAspect.java
@@ -3,11 +3,8 @@ package ar.edu.unq.desapp.grupoA.aspects;
 
 import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.stereotype.Component;
-import com.jcabi.aspects.Loggable;
 
 
 //@Component
@@ -17,11 +14,12 @@ public class LoggerAspect {
     private Logger log = Logger.getLogger(getClass());
 
     //busca todos los metodos publicos del paquete XX y que tengan la annotacion Loggeable
-  //  @After("execution(public * ar.edu.unq.desapp.grupoA.services.*.*(..)) && @annotation(Loggable)")
+    //  @After("execution(public * ar.edu.unq.desapp.grupoA.services.*.*(..)) && @annotation(Loggable)")
     //@After("execution(* *(..)) && @annotation(pablin)")
     //@After("execution (@Loggeable public * ar.edu.unq.desapp.grupoA.services.*.*(..))")
     @After("execution(public * ar.edu.unq.desapp.grupoA.services.*.*(..))")
-    public void logAfter(JoinPoint pjp, Loggable loggable) {
+    public void logAfter(JoinPoint pjp) {
+        //public void logAfter(JoinPoint pjp, Loggable loggable) {
         log.info(pjp.getSignature().getName() + " called...TESTING");
     }
 
--- src/main/java/ar/edu/unq/desapp/grupoA/models/Product.java
+++ src/main/java/ar/edu/unq/desapp/grupoA/models/Product.java
@@ -8,7 +8,6 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity
 @Table(name = "Product")
-@XmlRootElement(name = "product")
 public class Product {
 
     @Id()
